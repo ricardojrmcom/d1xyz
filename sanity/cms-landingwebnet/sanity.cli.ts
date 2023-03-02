2429ee3c4ef608520864a72644486c829ab6c4e6
ricardojrmcom <liber@ricardojrm.com>
2023-03-02 15:44:42 +0000

    fix: cms

--- sanity/cms-landingwebnet/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'm951qzp8',
-    dataset: 'production'
-  }
-})
--- sanity/cms-onlydragons/schemas/index.ts
+++
@@ -1,3 +0,0 @@
-import {author, video, imgBlog, blockContent, tag, item, collection} from '@d2xyz/cms/src/schemas'
-
-export const schemaTypes = [author, video, imgBlog, blockContent, tag, item, collection]
